88aef463f3bf6f45ef5456eea416b3f65f38ed23
marcelldac <dacmarcell@gmail.com>
2023-12-21 21:15:36 -0300

    faet: new discount policy

--- livro.rb
+++ livro.rb
@@ -1,36 +1,49 @@
 class Livro
-    attr_reader :titulo, :preco, :ano_lancamento
-    def initialize(titulo, preco, ano_lancamento)
+    attr_reader :titulo, :preco, :ano_lancamento, :possui_reimpressao
+    def initialize(titulo, preco, ano_lancamento, possui_reimpressao)
         @titulo = titulo
         @preco = preco
         @ano_lancamento = ano_lancamento
+        @possui_reimpressao = possui_reimpressao
         @preco = calcula_preco(preco)
     end
+    def possui_reimpressao?
+        @possui_reimpressao
+    end
     private
-
     def calcula_preco(base)
-        if @ano_lancamento < 2000
-            base * 0.7
+        if @ano_lancamento < 2006
+            if @possui_reimpressao
+                base * 0.9
+            else
+                base * 0.95
+            end
+        elsif @ano_lancamento <= 2010
+            base * 0.96
         else
             base
         end
     end
 end
-
-def livros_para_newsletter(livro)
-    if livro.ano_lancamento < 1999
-        puts "Newsletter/Liquidacao"
-        puts livro.titulo
-        puts livro.preco
+def livros_para_newsletter(livros)
+    livros.each do |livro|
+        if livro.ano_lancamento < 1999
+            puts livro.titulo
+            puts livro.preco
+            puts livro.possui_reimpressao?
+            puts "======================================"
+        end
     end
 end
-
 def aplica_promocoes(livro)
     if livro.ano_lancamento < 2000
         livro_preco *= 0.7
     end
 end
 
-algoritmos = Livro.new("Algoritmos", 100, 1998)
-
-livros_para_newsletter(algoritmos)
+algoritmosI = Livro.new("Algoritmos I", 100, 1998, true)
+algoritmosII = Livro.new("Algoritmos II", 150, 2000, false)
+thePragmaticProgrammer = Livro.new("The Pragmatic Programmer", 90, 1970, true)
+programmingRuby = Livro.new("Programming Ruby", 150, 2000, false)
+estoque = [algoritmosI, algoritmosII, thePragmaticProgrammer, programmingRuby]
+livros_para_newsletter(estoque)
